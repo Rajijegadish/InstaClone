100149a9b9104f62fdee107011bcf57af82ca347
Rajijegadish <147642252+Rajijegadish@users.noreply.github.com>
2023-12-23 20:34:38 +0530

    Update Story.js

--- src/timeline/Post/Story.js
+++ src/timeline/Post/Story.js
@@ -13,42 +13,44 @@ function Story() {
       {
         id: 0,
         name: " Wilfred69",
-        image: "img/first.jpg",
+        image: "https://mui.com/static/images/avatar/1.jpg",
       },
       {
         id: 1,
         name: "Oswaldo81",
-        image: "img/second.jpg",
+        image: "https://mui.com/static/images/avatar/3.jpg",
       },
       {
         id: 2,
         name: " Watson_Ritchie",
-        image: "img/third.jpg",
+        image: "https://mui.com/static/images/avatar/2.jpg",
       },
       {
         id: 3,
         name: " Jerrell_Thompson82",
-        image: "img/fourth.jpg",
+        image: "https://mui.com/static/images/avatar/4.jpg",
       },
       {
         id: 4,
         name: "  Margaret_Marquardt",
-        image: "img/five.jpg",
+        image: "https://mui.com/static/images/avatar/5.jpg",
       },
       {
         id: 5,
         name: "  Hadley77",
-        image: "img/seven.jpg",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2-C9Wobrs3EliqgWXTk--so1EoOxvcOg6MBHTHgmLnjnmM_yRVKaYAMc&usqp=CAU",
       },
       {
         id: 6,
         name: " Verlie_Carroll59",
-        image: "img/eight.jpg",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHnwhiwjXWxgqqK8KDxDQwlQTvTsADLPAtiA&usqp=CAU",
+",
       },
       {
         id: 7,
         name: " Jasen.Hilll29",
-        image: "img/nine.jpg",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPIBjWTFQfdgKyRPAd4gH2Tc6s_i6xuqSZPQ&usqp=CAU",
+",
       },
     ];
     setSelected(suggestion);
